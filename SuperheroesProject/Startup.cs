669103b39160a9ae08aec673df12eac6ce81829f
unknown <megdrescher@icloud.com>
2019-01-08 10:12:14 -0600

    thing

---
+++ SuperheroesProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperheroesProject.Startup))]
+namespace SuperheroesProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
